20d8d0e200e44e5db3ac726acdcbe2b63ed1c001
Leonan Lódula (Shacalliitto) <84084794+Shacalliitto@users.noreply.github.com>
2022-08-04 00:55:34 -0300

    Att da page
    Agora faz barulho de gato o botão

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const photo = document.getElementById('photos')
-function randomCat() {
+function meow() {
+    var audio = document.getElementById("audio");
+    audio.play();
     axios.get('https://api.thecatapi.com/v1/images/search')
         .then(function (response) {
             photo.setAttribute('src', response.data[0].url);
         });
 }
 
-randomCat()
+meow()
+
